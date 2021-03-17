586170efa0c0536c39aa729968fd7f249b27e183
Mario Niebes <github@niebes.net>
2021-03-17 18:12:30 +0100

    override metrics key

--- retrofit-metrics-micrometer/src/main/kotlin/net/niebes/retrofit/metrics/micrometer/MicrometerMetricsRecorder.kt
+++ retrofit-metrics-micrometer/src/main/kotlin/net/niebes/retrofit/metrics/micrometer/MicrometerMetricsRecorder.kt
@@ -2,23 +2,27 @@ package net.niebes.retrofit.metrics.micrometer
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
+import java.time.Duration
 import net.niebes.retrofit.metrics.MetricsRecorder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.time.Duration
 
-class MicrometerMetricsRecorder(private val meterRegistry: MeterRegistry) : MetricsRecorder {
+class MicrometerMetricsRecorder(
+    private val meterRegistry: MeterRegistry,
+    customMetricsKey: String?
+) : MetricsRecorder {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
+    private val metricsKey = customMetricsKey ?: DEFAULT_METRICS_KEY
 
     override fun recordTiming(tags: Map<String, String>, duration: Duration) {
-        log.info("measure {} with tags {} duration {}ms", METRICS_KEY, tags, duration.toMillis())
-        meterRegistry.timer(METRICS_KEY, asTags(tags)).record(duration)
+        log.info("measure {} with tags {} duration {}ms", metricsKey, tags, duration.toMillis())
+        meterRegistry.timer(metricsKey, asTags(tags)).record(duration)
     }
 
     companion object {
         private fun asTags(tags: Map<String, String>) =
             tags.map { Tag.of(it.key, it.value) }
 
-        const val METRICS_KEY = "http.client.requests"
+        const val DEFAULT_METRICS_KEY = "http.client.requests"
     }
 }
